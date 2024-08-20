5c2252d8966d5488313b11a27c232749b2bbeac6
Alex Raybosh <gopakita@protonmail.com>
2024-08-19 22:43:28 -0500

    zzz

--- darknet-nnpack/slave/slavetester.cc
+++ darknet-nnpack/slave/slavetester.cc
@@ -32,6 +32,7 @@ void readFull(int fd, void* buf, size_t size) {
 				throw std::runtime_error("error reading from "+std::to_string(fd)+": "+utils::errno_string());
 			}
 		}
+		if (ret==0) std::runtime_error("unexpected end of file");
 		pos+=ret;
 	}
 }
@@ -48,6 +49,7 @@ void writeFull(int fd, const void* buf, size_t size) {
 				throw std::runtime_error("error writing to "+std::to_string(fd)+": "+utils::errno_string());
 			}
 		}
+		if (ret==0) std::runtime_error("unexpectedly can't write");
 		pos+=ret;
 	}
 }
